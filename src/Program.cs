98e20143dbf42ae47d5fdac5bafb4ef599011f87
Kelvin <10376375+k30v1n@users.noreply.github.com>
2021-06-17 13:18:20 -0300

    fixing port configuration

--- src/Program.cs
+++ src/Program.cs
@@ -20,7 +20,10 @@ namespace app
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    var builder = webBuilder.UseStartup<Startup>();
+
+                    var port = Environment.GetEnvironmentVariable("PORT");
+                    builder.UseUrls("http://*:" + port);
                 });
     }
 }
